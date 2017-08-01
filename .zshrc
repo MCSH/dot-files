083cbe04fb9bdd9b12df3b4fa7b91bd2d7379155
Sajjad Heydari <mcshemail@gmail.com>
2017-08-01 20:17:13 +0430

    Initial zsh and setup

---
+++ .zshrc
@@ -0,0 +1,13 @@
+# Lines configured by zsh-newuser-install
+HISTFILE=~/.histfile
+HISTSIZE=10000
+SAVEHIST=1000
+setopt autocd
+bindkey -e
+# End of lines configured by zsh-newuser-install
+# The following lines were added by compinstall
+zstyle :compinstall filename '/home/sajjad/.zshrc'
+
+autoload -Uz compinit
+compinit
+# End of lines added by compinstall
---
+++ setup.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+FILES=".zshrc"
+
+DEST=$1
+
+if [ -z "$DEST" ]; then
+    DEST="$HOME"
+fi
+
+BASE=$(dirname $(readlink -f $0))
+
+ask_install(){
+    FILENAME=$1
+    
+    LINK="$DEST/$FILENAME"
+    TARGET="$BASE/$FILENAME"
+    
+    read -r -p "Link $LINK to $TARGET ?[y/N] " response
+    case $response in
+        [yY])
+        if [ -e $LINK ]; then
+            read -r -p "File exists, override? [y/N]" response
+            case $response in
+                [yY])
+                    rm -r -f "$LINK"
+                    ln -s "$TARGET" "$LINK"
+                    ;;
+            esac
+        else
+            ln -s "$TARGET" "$LINK"
+        fi
+        ;;
+    esac
+}
+
+for FILE in $FILES; do
+    ask_install $FILE
+done
