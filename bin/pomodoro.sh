514e9d1bf9d959090551fcd621df60ba23ea5520
Sajjad Heydari <mcshemail@gmail.com>
2018-12-14 15:05:48 +0330

    Added pomodor script

---
+++ bin/pomodoro.sh
@@ -0,0 +1,20 @@
+
+
+pomodor_wait(){
+    if [ -z "$1" ]
+    then
+        M=20
+    else
+        M=$1
+    fi
+
+    TIME=$(($M * 60))
+
+    notify-send "Pomodor session started for $M minutes"
+
+    sleep $TIME && notify-send "Pomodor session ended!" && aplay /home/sajjad/bin/bell.wav
+}
+
+export -f pomodor_wait
+
+nohup bash -c "pomodor_wait $1"  > /dev/null 2>&1 &
