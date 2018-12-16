bc823ae51941521d4defc7d092116ad5bfe202de
Sajjad Heydari <mcshemail@gmail.com>
2018-12-16 21:46:21 +0330

    migration on the new device!

--- bin/minecraft_server_excempt.sh
+++ bin/minecraft_server_excempt.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # script to make connections to irc.irchighway.net go via DEV.
-DEV=wlo1
-HOST=164.138.18.103
+DEV=wlp63s0
+HOST=64.141.32.241
 GW=$(ip route list | sed "s/.* via \([0-9.]*\) dev $DEV.*/\1/;t;d"|head -1)
+echo $GW
 #route add -host $HOST gw $GW $DEV
 ip route add $HOST dev $DEV via $GW
