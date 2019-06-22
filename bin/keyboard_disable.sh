46e10833ebcb0e5caee1dcd6e137e7e1a6325721
Sajjad Heydari <mcshemail@gmail.com>
2019-06-22 20:37:02 +0430

    Added disable keyboard

---
+++ bin/keyboard_disable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export KEYBOARD=14
+export MASTER=3
+
+xinput list | grep float
+if [ $? == 0 ]; then
+    notify-send "Enabling Keyboard"
+    xinput reattach $KEYBOARD $MASTER
+else
+    notify-send "Disabling Keyboard"
+    xinput float $KEYBOARD
+fi
