cd6951b41bc47bc3244733f61177481a1ef90c19
Sajjad Heydari <mcshemail@gmail.com>
2021-06-03 18:18:21 -0500

    updated monitor

--- bin/monitor_toggle.sh
+++ bin/monitor_toggle.sh
@@ -5,6 +5,8 @@ ROT=$(xrandr --query --verbose | grep "^DP1" | cut -d ' ' -f 5)
 
 if [ "$ROT" = "normal" ]; then
     xrandr --output DP1 --rotate left
+elif [ "$ROT" = "left" ]; then
+    xrandr --output DP1 --rotate right
 else
     xrandr --output DP1 --rotate normal
 fi
